7bb64440c71eb9edfefd488917a891a03a61c86c
RehanAhmad3553 <117620259+RehanAhmad3553@users.noreply.github.com>
2023-01-05 18:47:54 +0500

    Version2

--- app/src/main/java/com/example/volleyfetch/MainActivity.java
+++ app/src/main/java/com/example/volleyfetch/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.volleyfetch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Toast.makeText(this, "HelloWorld", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "HelloWorld", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "HelloWorld", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "HelloWorld", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "HelloWorld", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "HelloWorld", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "HelloWorld", Toast.LENGTH_SHORT).show();
 
 
     }
